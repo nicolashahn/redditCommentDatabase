2ca8b4e79fa82f24d390ccac8461a28dc65cbdad
Nicolas Hahn <nicolas@stonespring.org>
2015-07-31 12:31:43 -0700

    attempting to insert to server, not working yet

--- getRawJSON.py
+++ getRawJSON.py
@@ -1,16 +1,29 @@
 # Nicolas Hahn
-# in: Raw JSON in text format, one object per line
+# in: Raw Reddit JSON post objects in text format, one object per line
 # out: MySQL insertions for each object according to schema
 
 import json
 import sqlalchemy as s
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.dialects import mysql
 
 
+###################################
+# Global variables
+###################################
 
 # create base table class
 Base = declarative_base()
 
+# start unbound session - bind in main()
+Session = s.orm.sessionmaker()
+
+# because 1-5 appear to be taken in IAC
+reddit_id = 6
+
+###################################
+# JSON data manipulation
+###################################
 
 # json text dump -> list of json-dicts
 def jsonDataToDict(data):
@@ -21,6 +34,11 @@ def jsonDataToDict(data):
 		# print([x+": "+str(jline[x]) for x in sorted(jline)])
 	return jobjs
 
+
+###################################
+# MySQL setup helper functions
+###################################
+
 # open connection to database
 # then return engine object
 def connect(username, password, database):
@@ -29,49 +47,82 @@ def connect(username, password, database):
 	engine.connect()
 	return engine
 
-# general function to create tables
-# name = table name
-# cols = list of tuples = (name of col, type)
-def createTable(tname, cols):
-	__tablename__ = tname
-
-	for cname, ctype in cols:
-		if ctype.__class__ == 'int':
-			print cname
+# dataset = tinyint(3)
+# id = integer(20)
+# username = varchar(255)
+def createAuthor(dataset, id, username):
 
 	pass
 
-# dataset = int
-# id = int
-# username = varchar(30)
-def insertAuthor(dataset, id, username):
-	pass
 
+####################################
+# Table Classes
+####################################
 
 class Author(Base):
 	__tablename__ = 'authors'
 
-	dataset_id = s.Column(s.Integer, primary_key=True)
-	author_id = s.Column(s.Integer, primary_key=True)
-	username = s.Column(s.Varchar)
+	dataset_id = s.Column(mysql.TINYINT(3), primary_key=True)
+	author_id = s.Column(mysql.INTEGER(20), primary_key=True)
+	username = s.Column(mysql.VARCHAR(255))
 
 
+####################################
+# Table Tasks
+####################################
+
+# take a single json dictionary object
+# create table objects
+# insert to database
+def createTableObjects(jobj,session):
+	author = Author(
+		dataset_id = reddit_id, 
+		# sqlalchemy auto-increments primary keys by default
+		# author_id  = jobj[""]
+		username   = jobj["author"] 
+		)
+	print(jobj["author"])
+	# session - insert to server here
+	session.add(author)
+
+# general function to create tables
+# name = table name
+# cols = list of tuples = (name of col, type)
+# def createTable(tname, cols):
+# 	__tablename__ = tname
+
+# 	for cname, ctype in cols:
+# 		if ctype.__class__ == 'int':
+# 			print(cname)
+
+# 	pass
 
 def main():
-
-	# connect to server, bind metadata
-	eng = connect('root','nbhnbh','iac')
-	metadata = s.MetaData(bind=eng)
-	print(metadata)
 	
 	# raw text -> json dicts
 	data = open('sampleComments','r',)
 	jobjs = jsonDataToDict(data)
 
-	# print fields, types
-	( [print(x, jobjs[8][x].__class__) for x in sorted(jobjs[8])] )
+	# connect to server, bind metadata
+	eng = connect('root','nbhnbh','reddit')
+	metadata = s.MetaData(bind=eng)
+	print(metadata)
+
+	# uncomment to show all fields + types
+	# [print(x, jobjs[8][x].__class__) for x in sorted(jobjs[8])]
+	
+	# show fields with actual values
+	# [print(x, jobjs[8][x]) for x in sorted(jobjs[8])]
 	# what type is 'distinguished'? it's always null
 
+	# configure session, create session instance
+	Session.configure(bind=eng)
+	session = Session()
+
+	# now ready to start pushing to database
+	for jobj in jobjs:
+		createTableObjects(jobj,session)
+		session.commit()
 	
 
 if __name__ == "__main__":
