ce59e5847e243837aae62ee730cca602a7119aa5
Nicolas Hahn <nicolas@stonespring.org>
2015-08-24 12:35:20 -0700

    fixed structure sql script - python script running on server

--- redditStructure.sql
+++ redditStructure.sql
@@ -1,10 +1,11 @@
-insert into datasets (dataset_id = 6,name = 'reddit', source_url = 'http://www.reddit.com', description = 'link and image board');
+insert into datasets values(6, 'reddit', 'http://www.reddit.com', 'link and image board');
 
-create table subdreddits (
+create table subreddits (
 					dataset_id tinyint(3),
                     subreddit_id int(20) unsigned,
                     subreddit_name varchar(255),
-                    subreddit_native_id varchar(20)
+                    subreddit_native_id varchar(20),
+                    primary key(dataset_id, subreddit_id) 
                     );
 
 set sql_mode = 'STRICT_ALL_TABLES';
@@ -15,5 +16,3 @@ alter table posts change native_post_id native_post_id varchar(12);
 alter table discussions add subreddit_id int(10);
 
 alter table basic_markup add markup_group_id int(11);
- 
-# select * from subreddits;
